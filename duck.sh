a713dd9f296d0f58cce36e92385ba83cbe666cd4
Elvi Kalinoski <elvi.kalinoski@gmail.com>
2020-05-30 21:07:10 -0300

    Update duck.sh

--- duck.sh
+++ duck.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Variaveis para pegar os IPs
 myipv6=$(curl https://gtwy.net/ip/)
 myipv4=$(wget -qO- http://ipecho.net/plain)
 
+# Comando que envia para o servidor do duckdns
 echo url="https://www.duckdns.org/update?domains=exampledomain&token=a7c4d0ad-114e-40ef-ba1d-d217904a50f2&ip=$myipv4&ipv6=$myipv6" | curl -k -o ~/duckdns/duck.log -K -
